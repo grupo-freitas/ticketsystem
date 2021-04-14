845b0d0bfda2af5b64223acd9e9233845d9ff07d
Gustavo <gug.henri1@gmail.com>
2021-04-14 12:01:31 -0300

    Tela de cadastro

---
+++ js/showPassword.js
@@ -0,0 +1,26 @@
+let eye = document.querySelectorAll(".fa-eye-slash")
+let input = document.querySelectorAll(".password")
+let iconEye = document.querySelectorAll(".fa-eye-slash")
+
+for (let i = 0; i < input.length; i++) {
+  eye[i].addEventListener("mousedown", () => {
+    input[i].type = "text";
+    iconEye[i].className = "";
+    iconEye[i].className = "far fa-eye";
+  })
+}
+for (let i = 0; i < input.length; i++) {
+  eye[i].addEventListener("mouseup", () => {
+    input[i].type = "password";
+    iconEye[i].className = "";
+    iconEye[i].className = "far fa-eye-slash";
+  })
+}
+
+for (let i = 0; i < input.length; i++) {
+  eye[i].addEventListener("mousemove", () => {
+    input[i].type = "password";
+    iconEye[i].className = "";
+    iconEye[i].className = "far fa-eye-slash";
+  })
+}
