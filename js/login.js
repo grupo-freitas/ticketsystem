746365604b1b24b1787a3b82f778ac359f87256f
Gustavo <gug.henri1@gmail.com>
2021-04-20 11:45:46 -0300

    Sign up page

--- js/login.js
+++ js/login.js
@@ -1,9 +1,49 @@
-const form = document.querySelector('form')
+const login = document.querySelector('form')
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
+login.addEventListener('submit', async () => {
 
-  document.querySelector('.load').style.display = ' flex '
+  const login = document.querySelector('form input[name="login"]').value
+  const password = document.querySelector('form input[name="password"]').value
 
-  setTimeout(() => form.submit() , 2500)
+  const loginError = document.querySelector('#loginError')
+  const passwordError = document.querySelector('#passwordError')
+
+  let data = {
+    login,
+    password
+  }
+
+  const url = "http://localhost:3000/login"
+
+  const api = await fetch(url, {
+    method: "POST",
+    headers: {
+      'Accept': "application/json",
+      'Content-Type': "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+
+  const response = await api.json()
+
+  setTimeout(() =>{
+    if(response.msg === 'Login not exists'){
+      loginError.style.display = 'flex'
+      document.querySelector(".load").style.display = ' none '
+    }
+
+    if(response.msg === 'Invalid password'){
+      loginError.style.display = 'none'
+      passwordError.style.display = 'flex'
+      document.querySelector(".load").style.display = ' none '
+    }
+
+    if(response.msg === 'ok'){
+      const token = `bear ${response.token}`
+      window.location.href = "http://localhost:5500/home.html"
+      console.log(token)
+    }
+    
+  },1500)
+
 })
